ef54d11491c89edb121e08d93c6c4d348ed51c1d
Tim Sussman <timsussman1@gmail.com>
2025-12-04 09:53:33 -0500

    test

--- pedals.py
+++ pedals.py
@@ -1,26 +1,38 @@
 #!/usr/bin/python3
 from gpiozero import Button
+from time import time, sleep
 from subprocess import run
-from time import sleep
 
-btnA = Button(17, pull_up=True)
-btnB = Button(27, pull_up=True)
+btnA = Button(17, pull_up=True, bounce_time=0.3)
+btnB = Button(27, pull_up=True, bounce_time=0.3)
 
-def keypress(key):
-    print("Sending:", key)
-    run(["wtype", "-k", key])
+last_event_time = 0
+DEBOUNCE_TOTAL = 0.25   # Total debounce window in seconds
+
+def keypress(k):
+    print("Sending:", k)
+    run(["wtype", "-k", k])
 
 def A_released():
+    global last_event_time
+    now = time()
+    if now - last_event_time < DEBOUNCE_TOTAL:
+        return
+    last_event_time = now
     keypress("Down")
 
 def B_released():
+    global last_event_time
+    now = time()
+    if now - last_event_time < DEBOUNCE_TOTAL:
+        return
+    last_event_time = now
     keypress("Up")
 
 btnA.when_released = A_released
 btnB.when_released = B_released
 
-print("RUNNING SIMPLE TEST MODE")
-print("A = Down  |  B = Up")
+print("Stable test mode running… A=Down, B=Up")
 
 while True:
-    sleep(0.05)
+    sleep(0.01)
